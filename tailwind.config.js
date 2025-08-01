dcd7b6f585637012c1643b5edd71534f1fa7967d
wojo18 <wojti_ww@interia.pl>
2025-08-02 00:28:49 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
